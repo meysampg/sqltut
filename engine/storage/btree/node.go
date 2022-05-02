be6697d9535d7175bd362cfcd57b9e2d854ea60b
Meysam P. Ganji <p.g.meysam@gmail.com>
2022-05-02 11:18:57 +0430

    implement getters and setters for leafNode{NumCells,Key,Value} and implement initializeLeafNode

--- engine/storage/btree/node.go
+++ engine/storage/btree/node.go
@@ -48,30 +48,66 @@ const (
 	LeafNodeMaxCells      = LeafNodeSpaceForCells / LeafNodeCellSize
 )
 
-func leafNodeNumCells(order binary.ByteOrder, node []byte) uint32 {
-	return order.Uint32(node[LeafNodeNumCellsOffset : LeafNodeNumCellsOffset+LeafNodeNumCellsSize])
+func leafNodeNumCells(order binary.ByteOrder, node []byte) []byte {
+	return node[LeafNodeNumCellsOffset : LeafNodeNumCellsOffset+LeafNodeNumCellsSize]
 }
 
-func leafNodeCell(order binary.ByteOrder, node []byte, cellNum uint32) (uint32, *engine.Row) {
+func setLeafNodeNumCells(order binary.ByteOrder, node []byte, value uint32) {
+	order.PutUint32(leafNodeNumCells(order, node), value)
+}
+
+func getLeafNodeNumCells(order binary.ByteOrder, node []byte) uint32 {
+	return order.Uint32(leafNodeNumCells(order, node))
+}
+
+func leafNodeCell(order binary.ByteOrder, node []byte, cellNum uint32) ([]byte, []byte) {
 	return leafNodeKey(order, node, cellNum), leafNodeValue(order, node, cellNum)
 }
 
-func leafNodeKey(order binary.ByteOrder, node []byte, cellNum uint32) uint32 {
+func getLeafNodeCell(order binary.ByteOrder, node []byte, cellNum uint32) (uint32, *engine.Row) {
+	return getLeafNodeKey(order, node, cellNum), getLeafNodeValue(order, node, cellNum)
+}
+
+func setLeafNodeCell(order binary.ByteOrder, node []byte, cellNum uint32, key uint32, value *engine.Row) {
+	setLeafNodeKey(order, node, cellNum, key)
+	setLeafNodeValue(order, node, cellNum, value)
+}
+
+func leafNodeKey(order binary.ByteOrder, node []byte, cellNum uint32) []byte {
 	cell := node[offsetOfCell(cellNum):offsetOfCell(cellNum+1)]
 	key := cell[LeafNodeKeyOffset : LeafNodeKeyOffset+LeafNodeKeySize]
 
-	return order.Uint32(key)
+	return key
 }
 
-func leafNodeValue(order binary.ByteOrder, node []byte, cellNum uint32) *engine.Row {
+func setLeafNodeKey(order binary.ByteOrder, node []byte, cellNum uint32, value uint32) {
+	order.PutUint32(leafNodeKey(order, node, cellNum), value)
+}
+
+func getLeafNodeKey(order binary.ByteOrder, node []byte, cellNum uint32) uint32 {
+	return order.Uint32(leafNodeKey(order, node, cellNum))
+}
+
+func leafNodeValue(order binary.ByteOrder, node []byte, cellNum uint32) []byte {
 	cell := node[offsetOfCell(cellNum):offsetOfCell(cellNum+1)]
 	value := cell[LeafNodeValueOffset : LeafNodeValueOffset+LeafNodeValueSize]
 
-	return utils.Deserialize(order, value)
+	return value
 }
 
-func initializeLeafNode() {
+func getLeafNodeValue(order binary.ByteOrder, node []byte, cellNum uint32) *engine.Row {
+	return utils.Deserialize(order, leafNodeValue(order, node, cellNum))
+}
 
+func setLeafNodeValue(order binary.ByteOrder, node []byte, cellNum uint32, row *engine.Row) {
+	bytes := utils.Serialize(order, row)
+	offset := offsetOfCell(cellNum) + LeafNodeValueOffset
+
+	copy(bytes, node[offset:offset+LeafNodeValueSize])
+}
+
+func initializeLeafNode(order binary.ByteOrder, node []byte) {
+	setLeafNodeNumCells(order, node, 0)
 }
 
 func offsetOfCell(cell uint32) uint32 {
